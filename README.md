968f43843e5cc8257d84b5a11f620da23d213945
jayden-lee <gwanggi.lee@gmail.com>
2019-09-26 00:31:05 +0900

    init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring REST API Study Repo
+> 인프런 스프링 기반 REST API 개발 강좌를 학습하고 정리한 내용입니다 
---
+++ src/main/java/com/jayden/tutorial/springrestapi/SpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.jayden.tutorial.springrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApiApplication.class, args);
+    }
+
+}
