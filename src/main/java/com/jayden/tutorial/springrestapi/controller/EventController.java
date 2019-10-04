ff6c2f8d6af920c6a6e7b8275565fa7f134a8572
Jayden <jayden@chequer.io>
2019-10-04 17:49:57 +0900

    API 인덱스 만들기

--- src/main/java/com/jayden/tutorial/springrestapi/controller/EventController.java
+++ src/main/java/com/jayden/tutorial/springrestapi/controller/EventController.java
@@ -1,5 +1,6 @@
 package com.jayden.tutorial.springrestapi.controller;
 
+import com.jayden.tutorial.springrestapi.common.ErrorResource;
 import com.jayden.tutorial.springrestapi.events.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.Link;
@@ -36,12 +37,12 @@ public class EventController {
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
         if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
         eventValidator.validate(eventDto, errors);
 
         if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
@@ -56,5 +57,9 @@ public class EventController {
         eventResource.add(selfLinkBuilder.withRel("update-event"));
         eventResource.add(new Link("/docs/index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
+    }
+
+    private ResponseEntity<ErrorResource> badRequest(Errors errors) {
+        return ResponseEntity.badRequest().body(new ErrorResource(errors));
     }
 }
